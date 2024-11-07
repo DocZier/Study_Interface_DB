b3e0c2ac575650cc541df316c7d3328cef69f344
DocZier <doczier@gmail.com>
2024-11-08 00:40:36 +0300

    Resolved problem with remembered value of cell. But now i faced a new problem: Cursor not setting at the end of the cell. Trying to solve it

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -221,27 +221,25 @@ fun RowScope.EditableTableCell(
     isNumeric: Boolean
 ) {
     var isEditing by remember { mutableStateOf(false) }
-    var value by remember { mutableStateOf(TextFieldValue(text)) }
+    //var value by remember { mutableStateOf(TextFieldValue(text)) }
     val focusRequester = remember { FocusRequester() }
-
-
     if (isEditing) {
         LaunchedEffect(Unit) {
             focusRequester.requestFocus()
-            value = value.copy(selection = TextRange(value.text.length))
         }
         TextField(
-            value = value,
-            onValueChange = { newValue -> if (!isNumeric || newValue.text.all { it.isDigit() }) {
-                value = newValue
-            }},
+            value = text,
+            onValueChange = { newValue ->
+                if (!isNumeric || newValue.all { it.isDigit() }) {
+                    onValueChange(newValue)
+                }
+            },
             modifier = Modifier
                 .fillMaxHeight()
                 .border(1.dp, Color.Black)
                 .weight(weight)
                 .padding(8.dp)
-                .focusRequester(focusRequester)
-            ,
+                .focusRequester(focusRequester),
             singleLine = true,
             keyboardOptions = KeyboardOptions.Default.copy(
                 imeAction = ImeAction.Done,
@@ -249,13 +247,11 @@ fun RowScope.EditableTableCell(
             ),
             keyboardActions = KeyboardActions(onDone = {
                 isEditing = false
-                val finalValue = if (isNumeric && value.text.isEmpty()) "0" else value.text
-                onValueChange(finalValue)
             })
         )
     } else {
         Text(
-            text = value.text,
+            text = text,
             modifier = Modifier
                 .fillMaxHeight()
                 .border(1.dp, Color.Black)
@@ -265,6 +261,7 @@ fun RowScope.EditableTableCell(
         )
     }
 }
+
 
 @Composable
 fun AddModelDialog(
